055208bd23f1ea094b602b9e846bb8249079ba30
nam1601 <thongluc2401@gmail.com>
2022-10-04 19:32:36 +0700

    commit

--- src/pages/Profiles/index.js
+++ src/pages/Profiles/index.js
@@ -2,6 +2,7 @@ import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
 import { useEffect, useState } from 'react';
+import Button from '~/components/Button';
 
 import * as service from '~/services/searchService';
 import styles from './Profiles.module.scss';
@@ -32,8 +33,23 @@ function Profiles() {
                             {user.tick && <FontAwesomeIcon className={cx('check-icon')} icon={faCircleCheck} />}
                         </div>
                         <h1 className={cx('full-name')}>{user.first_name + ' ' + user.last_name}</h1>
+                        <Button outline className={cx('message-btn')}>
+                            Message
+                        </Button>
                     </div>
                 </div>
+                <div className={cx('introduces')}>
+                    <span>
+                        <strong>{user.followings_count}</strong> Followings
+                    </span>
+                    <span>
+                        <strong>{user.followers_count}</strong> Followers
+                    </span>
+                    <span>
+                        <strong>{user.likes_count}</strong> Likes
+                    </span>
+                </div>
+                <span className={cx('bio')}>{user.bio}</span>
             </div>
         </div>
     );
