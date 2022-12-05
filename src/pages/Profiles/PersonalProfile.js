a3b51f4c077bd5db794eccaf397e5bc6d39a7790
nam1601 <thongluc2401@gmail.com>
2022-12-06 00:46:01 +0700

    fix call api

---
+++ src/pages/Profiles/PersonalProfile.js
@@ -0,0 +1,12 @@
+import { faFileVideo } from "@fortawesome/free-solid-svg-icons";
+import classNames from "classnames/bind";
+
+import styles from './Profiles.module.scss';
+
+const cx = classNames.bind(styles);
+
+function PersonalProfile() {
+    return (  );
+}
+
+export default PersonalProfile;
