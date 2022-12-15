c6c250401cd2bf8f55c4ee70b2d0359cb979f8fb
nam1601 <thongluc2401@gmail.com>
2022-12-16 01:15:42 +0700

    change

--- src/untils/httpRequest.js
+++ src/untils/httpRequest.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
-import { useState } from 'react';
 
 const httpRequest = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL,
+    baseURL: 'https://tiktok.fullstack.edu.vn/api/',
     // headers: {
     //     'Content-Type': 'application/json',
     //     Authorization: 'Bearer ' + token,
